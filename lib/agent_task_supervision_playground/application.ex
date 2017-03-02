90bdb27322769fb3fad3d15e46e12efdcc324eff
Josh Adams <josh.rubyist@gmail.com>
2017-03-02 01:36:47 -0600

    Agents and Tasks and Supervisors, oh my

---
+++ lib/agent_task_supervision_playground/application.ex
@@ -0,0 +1,18 @@
+defmodule AgentTaskSupervisionPlayground.Application do
+  @moduledoc false
+
+  use Application
+  alias AgentTaskSupervisionPlayground.Bucket
+
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    children = [
+      supervisor(Task.Supervisor, [[name: OurSupervisor]]),
+      worker(Bucket, [OurBucket])
+    ]
+
+    opts = [strategy: :one_for_one, name: AgentTaskSupervisionPlayground.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
